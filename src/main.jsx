68152e9c797935b652771f3ebeb64d3c2cdb09ec
Jasper <buggy850903@gmail.com>
2026-03-17 17:09:58 +0800

    Add public share landing pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './i18n'
 import './index.css'
 import App from './App.jsx'
+import SharePage from './SharePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/share/space/:id" element={<SharePage type="space" />} />
+        <Route path="/share/beautician/:id" element={<SharePage type="beautician" />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
